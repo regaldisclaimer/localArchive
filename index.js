860d90f7a3a44f9371fa47db52fc0c35f0d12f8f
regaldisclaimer <regaldisclaimer@gmail.com>
2016-05-30 16:44:45 -0400

    initial packages

---
+++ index.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var fs   = require('fs');
+var ytdl = require('youtube-dl');
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+server.listen(80, '0.0.0.0');
+
+//app.get
+//playlist(queryUrl)
+
+
+function playlist(url) {
+
+  'use strict';
+  var video = ytdl(url);
+
+  video.on('error', function error(err) {
+    console.log('error 2:', err);
+  });
+
+  var size = 0;
+  video.on('info', function(info) {
+    size = info.size;
+    var output = path.join(__dirname + '/', size + '.mp4');
+    video.pipe(fs.createWriteStream(output));
+  });
+
+  var pos = 0;
+  video.on('data', function data(chunk) {
+    pos += chunk.length;
+    // `size` should not be 0 here.
+    if (size) {
+      var percent = (pos / size * 100).toFixed(2);
+      process.stdout.cursorTo(0);
+      process.stdout.clearLine(1);
+      process.stdout.write(percent + '%');
+    }
+  });
+
+  video.on('next', playlist);
+}
